e5192685e926759c108e1ae10047e2fd82c71fff
Alan Kersaudy <alan.kersaudy@gmail.com>
2021-10-24 08:43:46 +0200

    starting modal slot

---
+++ src/components/utils/Modal.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="darker">
+    <div class="modal">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Modal',
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.darker {
+  display: flex;
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.4);
+  z-index: 1;
+  align-items: center;
+  justify-content: center;
+}
+
+.modal {
+  display: block;
+  position: absolute;
+  margin: auto;
+  margin: 3rem;
+  border-radius: 1rem;
+  background-color: white;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <Modal>
+    <p>Eienn</p>
+  </Modal>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import Modal from '@/components/utils/Modal.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    Modal,
   },
 };
 </script>
