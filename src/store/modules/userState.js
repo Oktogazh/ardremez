2bec959487c921730b2539f4a6eebc97888da5b6
Alan Kersaudy <alan.kersaudy@gmail.com>
2021-10-26 13:01:39 +0200

    set user data mutation

--- src/store/modules/userState.js
+++ src/store/modules/userState.js
@@ -4,15 +4,32 @@ const userState = ({
   namespaced: true,
   state: {
     email: null,
+    customerId: null,
+    level: null,
+    subscribed: null,
     jwt: null,
+    verified: null,
   },
   mutations: {
+    SET_USER_DATA(state, data) {
+      const updatedData = { ...state, ...data };
+      state = updatedData; // eslint-disable-line no-param-reassign
+      localStorage.setItem('userData', JSON.stringify(state));
+      axios.defaults.headers.common.Authorization = `Bearer ${data.token}`;
+    },
   },
   actions: {
     login(context, { email, password }) {
       console.log(email, password);
       axios.post(`${context.rootState.api}/api/kevreañ`, { email, password })
-        .then((res) => console.log(res.data));
+        .then((res) => context.commit('SET_USER_DATA', {
+          email: res.data.email,
+          customerId: res.data.customerId,
+          level: res.data.live,
+          subscribed: res.data.sub,
+          jwt: res.data.token,
+          verified: res.data.verified,
+        }));
     },
     signin(context, { email, psw, confirmPsw }) {
       console.log(email, psw, confirmPsw);
