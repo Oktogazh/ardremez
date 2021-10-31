1f74640b13da814734d98e7abe4bde56d31989c6
Alan Kersaudy <alan.kersaudy@gmail.com>
2021-10-31 18:54:34 +0100

    Created a navigation guard in the router

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 import Home from '../views/Home.vue';
+// not the best way, but necessary in order to access the 'user/connected' getter
+// waiting for a better solution...
+import store from '../store';
 
 const routes = [
   {
@@ -8,18 +11,33 @@ const routes = [
     component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/library',
+    name: 'Library',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    component: () => import(/* webpackChunkName: "about" */ '../views/Library.vue'),
+    meta: {
+      requireAuth: true,
+    },
   },
 ];
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requireAuth)) {
+    if (store.getters['user/connected']) {
+      next();
+    } else {
+      next('/');
+    }
+  } else {
+    next();
+  }
 });
 
 export default router;
---
+++ src/views/Library.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="library">
+    <h1>This is the library</h1>
+  </div>
+</template>
