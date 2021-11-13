24d07f8a73f9f19709950876783cda9e80e5c7bc
Alan Kersaudy <alan.kersaudy@gmail.com>
2021-11-13 17:35:02 +0100

    read view downloads the chapter data, Reader.vue created

---
+++ src/components/read/Reader.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Reader',
+};
+</script>
+
+<style scoped>
+</style>
--- src/views/Read.vue
+++ src/views/Read.vue
@@ -1,12 +1,51 @@
 <template>
-  <div class="">
-
-  </div>
+  <Reader>
+  </Reader>
 </template>
-
 <script>
+import axios from 'axios';
+import Reader from '@/components/read/Reader.vue';
+
 export default {
   name: 'Read',
+  created() {
+    this.downloadChapter();
+  },
+  components: {
+    Reader,
+  },
+  data() {
+    return {
+      loading: false,
+      id: null,
+      chapterData: {
+        cards: [],
+        comeBack: null,
+        images: [],
+        links: [],
+        // announcement: [],
+      },
+      error: null,
+    };
+  },
+  methods: {
+    async downloadChapter() {
+      this.loading = true;
+      this.id = this.$route.query.p;
+      const { jwt } = this.$store.state.user;
+      const dataURI = `${this.$store.state.api}/api/lenn/${this.id}`;
+
+      const chapterData = await Promise.resolve()
+        .then(() => {
+          if (jwt) {
+            axios.defaults.headers.common.Authorization = `Bearer ${jwt}`;
+          }
+        })
+        .then(() => axios.get(dataURI))
+        .then((res) => res.data.kentel);
+      this.chapterData = chapterData;
+    },
+  },
 };
 </script>
 
