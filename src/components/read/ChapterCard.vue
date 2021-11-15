0aa296f16f2f0a57d8d75c60c73a70fa5f7fbf7c
Alan Kersaudy <alan.kersaudy@gmail.com>
2021-11-15 16:24:33 +0100

    toggle enabled, render list if caption

--- src/components/read/ChapterCard.vue
+++ src/components/read/ChapterCard.vue
@@ -1,10 +1,15 @@
 <template>
   <EmptyCard>
-    <template #header="/*{ toggle, bodyOpen }*/">
-      <h2 v-html="card.data.header"></h2>
+    <template #header="{ toggle }">
+      <div id="card-title" v-html="card.data.header"  @click="toggle"></div>
     </template>
     <template #body>
-      <pre>{{ card.data.body }}</pre>
+      <ul v-if="card.type === 'captions'">
+        <li v-for="(caption, index) in card.data.body"
+          :key="index" v-html="caption[0]" @click="play(caption[1])">
+        </li>
+      </ul>
+      <p v-else v-html="card.data.body"></p>
     </template>
   </EmptyCard>
 </template>
@@ -17,6 +22,11 @@ export default {
   components: {
     EmptyCard,
   },
+  methods: {
+    play(startTimestamp) {
+      return startTimestamp;
+    },
+  },
   props: {
     card: {
       required: true,
@@ -26,4 +36,11 @@ export default {
 </script>
 
 <style scoped>
+#card-title {
+  margin: .5em 1em;
+  font-size: 2em;
+  font-weight: 600;
+  cursor: pointer;
+}
+
 </style>
