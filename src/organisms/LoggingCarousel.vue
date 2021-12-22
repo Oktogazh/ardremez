b9436f5d06a631d0a43c5428fbdb6b99667e1ec9
Alan Kersaudy <alan.kersaudy@gmail.com>
2021-12-23 00:48:32 +0100

    org/LoggingCarousel: set what to do nex, deleted unused mol/forms/Form.vue

--- src/organisms/LoggingCarousel.vue
+++ src/organisms/LoggingCarousel.vue
@@ -7,6 +7,7 @@
       <GoBack @click="prev" />
       <PasswordForm v-if="member" :email="email" @authorized="closeThis"/>
       <NewPswForm v-else :email="email" @pwdApproved="signIn" />
+      <!-- TODO: add a reinitialize password component in a slot -->
     </template>
   </DualCarousel>
 </template>
@@ -66,25 +67,3 @@ export default {
   },
 };
 </script>
-
-<style scoped>
-.button {
-  cursor: pointer;
-}
-
-.access-container {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  position: absolute;
-  margin: auto;
-  padding: 5%;
-  border-radius: 2rem;
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: center;
-  box-shadow: 0 10px 15px rgba(54, 54, 54, 0.65);
-  background-image: url(/img/Beg_ar_Vann.jpg);
-}
-</style>
